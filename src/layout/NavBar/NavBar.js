2c6d6495512c2582cf2408489b6f07c4c83edac2
Ryan Santos <ryansantos86@gmail.com>
2019-02-18 15:32:14 -0800

    repo url

--- src/layout/NavBar/NavBar.js
+++ src/layout/NavBar/NavBar.js
@@ -6,6 +6,7 @@ import { NavBarWrap } from './NavBarStyles';
 import Icon from './../../components/common/icons/';
 import theme from './../../theme';
 import { actionCreators } from '../../actions/RoverActions';
+import { GITHUB_REPO_URL } from './../../constants';
 
 class NavBar extends Component {
   toggleMenu = async () => {
@@ -20,7 +21,11 @@ class NavBar extends Component {
           width={25}
           fill={theme.primary}
           iconClick={this.toggleMenu} />
-        <Icon name="github" width={25} fill={theme.primary} />
+        <Icon
+          name="github"
+          width={25}
+          fill={theme.primary}
+          iconClick={() => window.open(GITHUB_REPO_URL, '_blank')} />
       </NavBarWrap>
     );
   }
