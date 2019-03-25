645e36313e44e7b5c1db128209eb55b31517dea2
Ryan Santos <ryansantos86@gmail.com>
2019-03-25 11:05:19 -0700

    commit

--- src/constants.js
+++ src/constants.js
@@ -37,8 +37,7 @@ const CAMERAS = [
   }
 ];
 
-const API_KEY = 'ckPZgbeexCCXZb1IOIPsmugP5Uq3rowfH86GI6qM';
 const BASE_API_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers';
 const GITHUB_REPO_URL = 'https://github.com/ryasan86/mars_rovers';
 
-export { BASE_API_URL, CAMERAS, API_KEY, GITHUB_REPO_URL };
+export { BASE_API_URL, CAMERAS, GITHUB_REPO_URL };
