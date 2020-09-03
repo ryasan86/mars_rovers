eaa0673eb03eefb9b9be5dd3e0e75ff8a85debf8
Ryan Santos <ryansantos86@gmail.com>
2020-09-03 11:27:40 -0700

    Updates

---
+++ precache-manifest.8c2d6c50f181539311cf2ddfdc0b468b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1552d230cf3898ee639b87c82f3d6341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a021276a822129ae60",
+    "url": "/static/css/2.8a9e15d2.chunk.css"
+  },
+  {
+    "revision": "48317ac179f8c6d0d4ae",
+    "url": "/static/css/main.80d0d99f.chunk.css"
+  },
+  {
+    "revision": "88a021276a822129ae60",
+    "url": "/static/js/2.370ca472.chunk.js"
+  },
+  {
+    "revision": "c3cb654a3a7d27a576aed8d503b856d8",
+    "url": "/static/js/2.370ca472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48317ac179f8c6d0d4ae",
+    "url": "/static/js/main.5dc7eadf.chunk.js"
+  },
+  {
+    "revision": "44865a7fb4db94287330",
+    "url": "/static/js/runtime-main.ed9cc4a2.js"
+  },
+  {
+    "revision": "0463621cd948862b0dd1826001f01270",
+    "url": "/static/media/curiosity-1.0463621c.png"
+  },
+  {
+    "revision": "766b8620df7e5d900bdd0eb1d4b89f67",
+    "url": "/static/media/marsRoverClip.766b8620.mp4"
+  },
+  {
+    "revision": "8458c2e9ffaee4516dcaeab68b92ce45",
+    "url": "/static/media/milkyWay.8458c2e9.mp4"
+  },
+  {
+    "revision": "9ff66f7f5209e9dbabb65f29c239e84e",
+    "url": "/static/media/opportunity-1.9ff66f7f.png"
+  },
+  {
+    "revision": "1acb1d64cbbecaa38e8b2a470a16e9bc",
+    "url": "/static/media/perseverance.1acb1d64.webm"
+  },
+  {
+    "revision": "411b49b91708338369b6b9f408ea86a9",
+    "url": "/static/media/spaceship.411b49b9.gif"
+  },
+  {
+    "revision": "2fbe9a273ed63cd9a227bd803b3da834",
+    "url": "/static/media/spirit-1.2fbe9a27.png"
+  }
+]);
